53ccf81215fbd934dcd7f4353f0e88325d8423ed
Jason Stone <jason.stone0808@gmail.com>
2018-11-07 03:27:35 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  rooms = ["kitchen", "bedroom", "bathroom", "livingroom"]
+  ["kitchen", "bedroom", "bathroom", "livingroom"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+
 end
 
 def add_element_to_start_of_array(array, element)
