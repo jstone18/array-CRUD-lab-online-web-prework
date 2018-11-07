50e95a94f48428ddee7f2c98dd17e85f92e29dbb
Jason Stone <jason.stone0808@gmail.com>
2018-11-07 03:26:16 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  rooms.pop "playroom"
-  puts rooms.inspect
+  
 end
 
 def add_element_to_start_of_array(array, element)
